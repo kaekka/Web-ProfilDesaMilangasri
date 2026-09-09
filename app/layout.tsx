47edd4b014329734332ab25d5229539c53d00551
Farouq <farouqga@gmail.com>
2026-09-09 15:28:09 +0700

    fix: add suppressHydrationWarning to html element for browser extension compatibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="id"
       className="scroll-smooth"
+      suppressHydrationWarning
     >
       <head>
         <link
